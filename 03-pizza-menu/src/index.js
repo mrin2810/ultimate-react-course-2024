24dd85e0d4209bd5fd057532d07db26f4d55a636
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2024-04-22 17:29:48 -0400

    Ternary operator for conditional rendering

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -84,12 +84,14 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => {
             return <Pizza pizzaObj={pizza} key={pizza.name} />;
           })}
         </ul>
+      ) : (
+        <p>We're working on our menu. Please come back later :)</p>
       )}
     </main>
   );
@@ -104,11 +106,15 @@ function Footer() {
   console.log(isOpen);
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
       )}
     </footer>
   );
