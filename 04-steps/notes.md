8da950f572b033b0bfccc6f7c5ee35806465859d
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2024-04-23 11:52:57 -0400

    State for step

---
+++ 04-steps/notes.md
@@ -0,0 +1,16 @@
+# State in React
+
+- What is state?
+
+  - Data is of 2 types in a react component. We already learnt about Props, now we will learn about State.
+  - State is the memory of a component.
+  - Notification count, active tab, content of shopping cart
+  - State can be easily changed
+  - Data need to be held over the lifecycle of the data
+  - State variable is one piece of state, and State means all the states in the component.
+  - Updating a state triggers rerendering the component in the DOM.
+  - Persists local variables between renders.
+
+- How to use state in practice?
+  - We will use it in practice.
+- How to think about states in React?
--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,8 +7,15 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
 
+  function handlePrevious() {
+    setStep(step - 1);
+  }
+
+  function handleNext() {
+    setStep(step + 1);
+  }
   return (
     <div className="steps">
       <div className="numbers">
@@ -14,14 +23,22 @@ export default function App() {
         <div className={`${step >= 2 ? "active" : ""}`}>2</div>
         <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
+
       <p className="message">
         Step {step}: {messages[step - 1]}
       </p>
+
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
