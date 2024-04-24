a9357497d429e9958a66ae4caf3d3c0cf859ac4a
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2024-04-24 10:57:17 -0400

    React Controls the Form elements instead of DOM

---
+++ 05-travel-list/notes.md
@@ -0,0 +1,13 @@
+# Working with Controlled Elements
+
+- We can access the data in forms, using event object.
+- But in react we use Controlled Elements to do that.
+- With this, it is React who controls the state of the elements in the form and not DOM.
+
+# 3 Step Approach
+
+1. Create a piece of state (e.g. description)
+2. Use the state as the value of the input element (e.g. value={description})
+3. listen for the change event using onChange prop (e.g. onChange={e => setDescription(e.target.value)})
+
+- These steps will let React control the form element instead of DOM.
--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -20,24 +22,38 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDiscription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+  }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for you 😍 trip?</h3>
-      <select>
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
         {Array.from({ length: 20 }, (_, num) => num + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..."></input>
+      <input
+        type="text"
+        value={description}
+        placeholder="Item..."
+        onChange={(e) => setDiscription(e.target.value)}
+      ></input>
+      <button>Add</button>
     </form>
   );
 }
 
 function Item({ item }) {
   return (
-    <li key={item.id}>
+    <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
@@ -51,7 +67,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item key={item.id} item={item} />
         ))}
       </ul>
     </div>
