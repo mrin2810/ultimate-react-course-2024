1a776b9a59933e2686165f3ee55feb861b718238
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2024-04-24 10:24:31 -0400

    Skeleton Code

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,6 +1,42 @@
-import "./App.css";
-function App() {
-  return <div className="App">Hello</div>;
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
-export default App;
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for you 😍 trip?</h3>
+    </div>
+  );
+}
+
+function Item() {
+  return <h4>Item Name</h4>;
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <Item />
+    </div>
+  );
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em>💼 You have X items on your list and you already packed Y (Z%)?</em>
+    </footer>
+  );
+}
