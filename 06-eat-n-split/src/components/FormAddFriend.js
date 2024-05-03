25374d7028945c24c37ec983dff95f2bd540ac81
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2024-05-03 14:25:16 -0400

    Form Add Friend Done

--- 06-eat-n-split/src/components/FormAddFriend.js
+++ 06-eat-n-split/src/components/FormAddFriend.js
@@ -1,13 +1,34 @@
+import { useState } from "react";
 import Button from "./Button";
 
 export default function FormAddFriend() {
+    const [name, setName] = useState("");
+    const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+    function handleSubmit(e) {
+      e.preventDefault();
+
+      if (!name || !image) return;
+      const id = crypto.randomUUID();
+      const newFriend = {
+        name,
+        image: `${image}?=${id}`,
+        balance: 0,
+        id,
+      };
+
+      console.log(newFriend);
+
+      setName("");
+      setImage("https://i.pravatar.cc/48")
+    }
     return (
-      <form className="form-add-friend">
+      <form className="form-add-friend" onSubmit={handleSubmit}>
         <label>👫 Friend name</label>
-        <input type="text" />
+        <input value={name} onChange={(e) => setName(e.target.value)} type="text" />
   
         <label>🌅 Image Url</label>
-        <input type="text" />
+        <input value={image} onChange={(e) => setImage(e.target.value)} type="text" />
   
         <Button>Add</Button>
       </form>
