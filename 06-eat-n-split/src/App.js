8afa1d015e21ca6ee89c360f360d01752d312e70
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2024-05-03 11:39:15 -0400

    Rearrranged files

--- 06-eat-n-split/src/App.js
+++ 06-eat-n-split/src/App.js
@@ -1,23 +1,7 @@
-const initialFriends = [
-  {
-    id: 118836,
-    name: "Clark",
-    image: "https://i.pravatar.cc/48?u=118836",
-    balance: -7,
-  },
-  {
-    id: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
-    balance: 20,
-  },
-  {
-    id: 499476,
-    name: "Anthony",
-    image: "https://i.pravatar.cc/48?u=499476",
-    balance: 0,
-  },
-];
+import FriendsList from "./components/FriensList";
+import FormAddFriend from "./components/FormAddFriend";
+import Button from "./components/Button";
+import FormSplitBill from "./components/FormSplitBill";
 
 export default function App() {
   return (
@@ -34,77 +18,8 @@ export default function App() {
   );
 }
 
-function FriendsList() {
-  const friends = initialFriends;
-  return (
-    <ul>
-      {friends.map((friend) => (
-        <Friend key={friend.id} friend={friend} />
-      ))}
-    </ul>
-  );
-}
 
-function Friend({ friend }) {
-  return (
-    <li>
-      <img src={friend.image} alt={friend.name} />
-      <h3>{friend.name}</h3>
-      {friend.balance < 0 && (
-        <p className="red">
-          You owe {friend.name} ${Math.abs(friend.balance)}
-        </p>
-      )}
-      {friend.balance > 0 && (
-        <p className="green">
-          {friend.name} owes you ${Math.abs(friend.balance)}
-        </p>
-      )}
-      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-      <Button>Select</Button>
-    </li>
-  );
-}
 
-function Button({ children }) {
-  return <button className="button">{children}</button>;
-}
 
-function FormAddFriend() {
-  return (
-    <form className="form-add-friend">
-      <label>👫 Friend name</label>
-      <input type="text" />
 
-      <label>🌅 Image Url</label>
-      <input type="text" />
 
-      <Button>Add</Button>
-    </form>
-  );
-}
-
-function FormSplitBill() {
-  return (
-    <form className="form-split-bill">
-      <h2>Split a bill with X</h2>
-
-      <label>💰 Bill Value</label>
-      <input type="text" />
-
-      <label>🧍‍♂ Your Expense</label>
-      <input type="text" />
-
-      <label>👫 X's Expense</label>
-      <input type="text" disabled/>
-      
-      <label>👫 Who is paying the bill?</label>
-      <select>
-        <option value="user">You</option>
-        <option value="friend">X</option>
-      </select>
-       
-      <Button>Split bill</Button>
-    </form>
-  );
-}
