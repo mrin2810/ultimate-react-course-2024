0d110967007dbb10386501b097ef40f4164646d6
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2024-05-04 13:57:26 -0400

    Moving everything to their own file

---
+++ 07-usepopcorn/src/Components/NavBar.js
@@ -0,0 +1,13 @@
+import Search from "./Search";
+import Logo from "./Logo";
+import NumResults from "./NumResults";
+
+export default function NavBar({ movies }) {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      <Search />
+      <NumResults movies={movies} />
+    </nav>
+  );
+}
