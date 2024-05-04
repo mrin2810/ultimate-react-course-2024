ef471baf6b2c765789ea4aecda77a87c53145ab4
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2024-05-04 18:27:01 -0400

    Used Composition to avoid prop drilling

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -2,13 +2,28 @@ import { useState } from "react";
 import NavBar from "./Components/NavBar";
 import Main from "./Components/Main";
 import { tempMovieData } from "./data/tempMovieData";
+import Logo from "./Components/Logo";
+import Search from "./Components/Search";
+import NumResults from "./Components/NumResults";
+import ListBox from "./Components/ListBox";
+import WatchedBox from "./Components/WatchedBox";
+import MovieList from "./Components/MovieList";
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   return (
     <>
-      <NavBar movies={movies} />
-      <Main movies={movies} />
+      <NavBar>
+        <Logo />
+        <Search />
+        <NumResults movies={movies} />
+      </NavBar>
+      <Main>
+        <ListBox>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
