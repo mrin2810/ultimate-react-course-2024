29ee30ce379be0e8aca5b32abb7a3eda645f9d1a
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2024-05-04 13:43:49 -0400

    lifted movies up to app

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -91,25 +91,26 @@ function NavBar() {
   );
 }
 
-function Main() {
+function Main({ movies }) {
   return (
     <main className="main">
-      <ListBox />
+      <ListBox movies={movies} />
       <WatchedBox />
     </main>
   );
 }
 
 export default function App() {
+  const [movies, setMovies] = useState(tempMovieData);
   return (
     <>
       <NavBar />
-      <Main />
+      <Main movies={movies} />
     </>
   );
 }
 
-function ListBox() {
+function ListBox({ movies }) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -120,13 +121,12 @@ function ListBox() {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieList />}
+      {isOpen1 && <MovieList movies={movies} />}
     </div>
   );
 }
 
-function MovieList() {
-  const [movies, setMovies] = useState(tempMovieData);
+function MovieList({ movies }) {
   return (
     <ul className="list">
       {movies?.map((movie, i) => (
